0db9b139dbe69f8e0e423cdea24c45778f445b10
Sergey <tolshin.sergei@gmail.com>
2020-10-21 15:34:12 +0500

    YaMDB

--- api/filters.py
+++ api/filters.py
@@ -6,7 +6,7 @@ class TitleFilter(filters.FilterSet):
     category = filters.CharFilter(field_name='category__slug', lookup_expr='iexact')
     genre = filters.CharFilter(field_name='genre__slug', lookup_expr='iexact')
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    year = filters.NumberFilter(field_name='year', lookup_expr='exact') 
+    year = filters.NumberFilter(field_name='year', lookup_expr='exact')
 
     class Meta:
         model = Title
