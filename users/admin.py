cfb3839be705b1c22fc2f3a3c6d459c508cb7fba
Sergey <tolshin.sergei@gmail.com>
2020-10-21 15:26:58 +0500

    YaMDB

--- users/admin.py
+++ users/admin.py
@@ -11,8 +11,7 @@ class UserAdmin(BaseUserAdmin):
         (_('Personal info'), {
          'fields': ('first_name', 'last_name', 'email', 'bio')}),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 
-            'groups', 'user_permissions', 'role'),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'role'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
