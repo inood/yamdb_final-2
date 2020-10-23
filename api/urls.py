6247c778e58e8be91a64485180194df7bcb979b9
Sergey <tolshin.sergei@gmail.com>
2020-10-23 09:34:05 +0500

    YaMDB

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,6 @@ router.register(
 router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
 router.register('titles', TitleViewSet)
-router.register('blablabla', TitleViewSet)
 
 
 urlpatterns = [
